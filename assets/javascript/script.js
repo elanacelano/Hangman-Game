31a5a61f19b80a16baf52d446126f96a0368332d
Elana Celano <elanacelano@gmail.com>
2017-04-13 15:32:18 -0400

    Second attempt at Hangman

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,4 @@
-      // what are the varhiables 
+  // what are the varhiables 
     var hangmanGame = { //opening hangmanGame object
       hangmanState: -1,
       gameAnswer: "Boolean",
@@ -6,13 +6,22 @@
       // how many guesses
       gameAnswer: function() {
         document.onkeyup = function(event) {
-    	alert(event);
-	};
-//       if (x === "B" || x === "O" || x === "O" || 
-//           x === "L" || x === "E" || x === "A" || x === "N") { 
-//         alert ("You pressed the 'A' key!");
-//     }
-// }
+    	alert(event);{
+       if (x === "B" || x === "O" || x === "O" || 
+           x === "L" || x === "E" || x === "A" || x === "N") { 
+      alert ("You pressed the 'B' key");("You pressed the 'O' key");
+    ("You pressed the 'O' key");("You pressed the 'E' key!");
+    ("You pressed the 'A' key!");("You pressed the 'N' key!");
+    }
+ }     if (i === "Boolean" is guessed);
+
+        function wordBox(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z){
+        console.log(boolean === guessed)
+      }
+        wordBox(a,b,c...)
+  } 
+     if (i === "Boolean" is guessed);
+ console.log([Boolean]);
 
       }
     }// closing hangmanGame object
